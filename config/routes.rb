9161d03159860fb719253110072687adc3aaf434
johni1228 <akirus1228@gmail.com>
2021-10-05 22:03:16 +0900

    fix: router

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { sessions: :sessions },
                        path_names: { sign_in: :login }
 
-
+                 
     resource :user, only: [:show, :update]
 
     resources :profiles, param: :username, only: [:show] do
